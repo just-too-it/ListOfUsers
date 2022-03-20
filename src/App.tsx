4cd77a5ed84f6d5ca9f75722b21fd36ac7768808
Natasha <just.too.it@gmail.com>
2022-03-20 21:01:25 +0300

    Добавлен компонент индикатора загрузки

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React, { FC, useEffect, useState } from 'react';
+
 import { BrowserRouter } from 'react-router-dom';
+
+import { Loader } from './components/Loader';
 
 import { Sidebar } from './components/Sidebar';
 import { IUserItem } from './components/UserItem/UserItem.types';
@@ -9,19 +12,24 @@ import AppRouter from './routes';
 
 export const App: FC = (): React.ReactElement => {
   const [users, setUsers] = useState([] as IUserItem[]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchUsers(): Promise<void> {
+    setIsLoading(true);
+    const users = await getUsers();
+    setUsers(users);
+    setIsLoading(false);
+  }
 
   useEffect(() => {
-    void getUsers().then((data) => {
-      setUsers(data as IUserItem[]);
-      return null;
-    });
+    void fetchUsers();
   }, []);
 
   return (
     <usersContext.Provider value={[users, setUsers]}>
       <BrowserRouter>
         <Sidebar />
-        <AppRouter />
+        {isLoading ? <Loader /> : <AppRouter />}
       </BrowserRouter>
     </usersContext.Provider>
   );
--- src/core/services/users.service.ts
+++ src/core/services/users.service.ts
@@ -1,12 +1,14 @@
-import { IUserItem } from "../../components/UserItem/UserItem.types";
+import { IUserItem } from '../../components/UserItem/UserItem.types';
+import axios from 'axios';
 
 export const getUsers = async (): Promise<IUserItem[] | undefined> => {
-    const url = 'https://jsonplaceholder.typicode.com/users';
-    const response = await fetch(url);
-    try {
-      const parseResponse = await response.json();
-      return parseResponse as IUserItem[];
-    } catch (error) {
-      alert(error);
-    }
-  };
+  const url = 'https://jsonplaceholder.typicode.com/users';
+
+  try {
+    const response = await axios.get(url);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return response.data;
+  } catch (error) {
+    alert(error);
+  }
+};
